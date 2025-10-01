3a298350e6593da8ffaa9445f7db60ddc9889b3b
Femiolajide <131526696+Femiolajide@users.noreply.github.com>
2025-10-01 23:50:20 +0100

    updated

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Excel Fuctions in Python
+# 📈 **Excel Functions in Python**
+![alt text](screenshot.png)
 
+
+This project is about recreating some common Excel functions in Python. The idea is simple: many people are used to working in Excel, but Python is more powerful for larger projects.  So I decided to show how Excel-style functions can be written and used in Python.
+
+---
+## 📑 **Table of Contents**
+1. [Introduction](#excel-function-in-python)  
+2. [Why I Built This](#-why-i-built-this)  
+3. [What’s Inside](#-whats-inside)  
+    - [Math & Stats Functions](#-math-&-stats-functions)
+        - [SUM](#1.-sum())
+
+
+## 🔹 **Why I Built This**
+- To practice and show my coding skills in Python.  
+- To make it easier for people who already know Excel to see how things translate into Python.  
+- To create a starting point for a bigger portfolio where I’ll keep adding useful functions and projects.  
+
+---
+
+## 🔹 **What’s Inside**
+The functions are grouped into:
+### 🧮 **Math & Stats Functions**
+####  **1. SUM()**
+---
+
+- **Description:**  
+    - Implemented as `excel_sum(*numbers)` in Python.  
+    - Replicates Excel’s `=SUM()` behaviour:  
+        - Accepts lists, tuples, Series, or DataFrames as ranges.  
+        - Ignores logical values (`TRUE` / `FALSE`) and text inside ranges, just like Excel.  
+        - Raises `TypeError` for invalid inputs (similar to Excel’s `#NAME?` error).  
+        - Can handle more than 255 arguments, unlike Excel’s limit.  
+
+
+
+- **How Excel Shows It:**  
+This is how Excel presents the SUM function in its *Function Arguments* window:  
+
+![ex2.png](attachment:ex2.png)
+
+
+
+**Source Code:**  
+[👉 View implementation in Python](functions.py#L10-L25)  
+
+
+
+**Notebook Demo:**  
+[📓 See usage example in the demo notebook](demo_notebook.ipynb)  
---
+++ excel_math_function.py
@@ -0,0 +1,97 @@
+def excel_sum(*numbers):
+    """**Excel**
+    Add all the numbers in a range of ceels
+    
+**Python**
+    Add all the numbers in a container (by container, 
+    I mean data structure like list, tuple, dataframe,
+    and series) as a representation of range of cells
+    
+**Parameters**
+    -------------------------------------------------------------
+        **numbers**: int, float, list, tuple, pandas serues or datafram
+        number1,number2....(number to sum is not limited unlike excel's
+        that allows only from 1 to 255). Logical values and texts are 
+        ignored in containers (list, tuple, series and dataframe), but
+        included if typed as arguments.
+**Returns**
+    -------------------------------------------------------------
+        **int or float**
+        
+**Raises**
+    -------------------------------------------------------------
+        **TypeError**
+        Raises TypeError if the arguments contain unrecognized string
+
+**Note**
+    -------------------------------------------------------------
+    This is purely logical representation of how sum function behaves 
+    in excel.
+    """
+    # importing the required packages
+    import numpy as np
+    import pandas as pd 
+    # Separating the parameters into different data structure/type
+    num_text = [] # append if type is str, int or float
+    from_cont = [] # unpack all the elements in list or tuple into this
+    from_pandas = [] # unpack all the elements in dataframe or series into this
+    for x in numbers:
+        if isinstance(x,(str,float,int)):
+            num_text.append(x)
+        elif isinstance(x,(tuple,list)):
+            from_cont.extend(x)
+        elif isinstance(x,(pd.DataFrame,pd.Series)):
+            # Trying to do some cleanup here
+            # Replacing all logical values (True or False) with 0
+            no_logicals = x.map(lambda x: 0 if x == True or x == False
+                                else x)
+            # Converting the outcome to numpy then to list 
+            no_logicals = no_logicals.to_numpy()
+            # An attempt to reshape dataframe to (n,) form
+            no_logicals = no_logicals.flatten()
+            # Converting numpy to list 
+            no_logicals = list(no_logicals)
+            # unpacking the contents into from_pandas 
+            from_pandas.extend(no_logicals)
+        else:
+            # Let the users know that their datatype is not allowed
+            print(f"{type(x)} object is not allowed")
+            break
+    # Trying to do some cleanup here also
+    # Replacing all logical values (True or False) in from_cont with 0
+    from_cont = [0 if x == True or x == False else x for x in from_cont]
+    # Joining from_cont and from_pandas together 
+    container = from_cont + from_pandas
+    # An attempt to exclude all unrecognized text in container
+    clean_cont = []
+    for x in container:
+        try:
+            clean_cont.append(float(x))
+        except ValueError:
+            pass 
+    # Joining clean_cont and num_text together 
+    all_nums = clean_cont + num_text
+    # An attempt to cast all string-like numbers in all_nums to float
+    clean_all_nums = []
+    for x in all_nums:
+        try:
+            clean_all_nums.append(float(x))
+        except ValueError:
+            clean_all_nums.append(x)
+    # Error handling and finalizing the answer
+    if all([isinstance(x,(int,float)) for x in clean_all_nums]):
+        if sum(clean_all_nums) == int(sum(clean_all_nums)):
+            return int(sum(clean_all_nums))
+        else:
+            return sum(clean_all_nums)
+    else:
+        raise TypeError ("#NAME?\nThe arguments contain unrecognized text")
+        
+
+
+
+
+
+
+
+    
